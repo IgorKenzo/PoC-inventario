80135116bc523c46ac8f81adf1d9c431b531d3bf
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2019-09-21 19:16:41 -0300

    Add no inventario

--- PoC-Inventario/main.c
+++ PoC-Inventario/main.c
@@ -6,9 +6,14 @@
 // Inclui a coisa do mouse
 #include <allegro5/allegro_native_dialog.h>
 
+#include <allegro5/color.h>
+#include <allegro5/allegro_primitives.h>
+
 // Tamanho da tela
-#define LARGURA_TELA 640
-#define ALTURA_TELA 480
+#define LARGURA_TELA 900
+#define ALTURA_TELA 700
+// Tamanho Inventario
+#define TAMANHO_INVENTARIO 2
 
 typedef struct Objeto
 {
@@ -21,9 +26,11 @@ typedef struct Objeto
 	int cliqueY;
 } Objeto;
 
-int main(void) {
-	Objeto invetario[4][2];
+Objeto inventario[TAMANHO_INVENTARIO];
 
+int main(void) {
+	
+	int invCount = 0, c;
 
 	ALLEGRO_DISPLAY* janela = NULL;
 
@@ -49,6 +56,27 @@ int main(void) {
 	certo.largura = 100;
 	certo.altura = 100;
 
+	Objeto lobinho;
+	lobinho.bitmap = NULL;
+	lobinho.x = LARGURA_TELA/2 - 200;
+	lobinho.y = 0;
+	lobinho.largura = 200;
+	lobinho.altura = 252;
+
+	Objeto canudo;
+	canudo.bitmap = NULL;
+	canudo.x = LARGURA_TELA - 175;
+	canudo.y = 0;
+	canudo.largura = 175;
+	canudo.altura = 279;
+
+	Objeto aquarela;
+	aquarela.bitmap = NULL;
+	aquarela.x = 0;
+	aquarela.y = ALTURA_TELA/2 - 256;
+	aquarela.largura = 256;
+	aquarela.altura = 256;
+
 	//declara a fila de eventos
 	ALLEGRO_EVENT_QUEUE* fila_eventos = NULL;
 
@@ -58,7 +86,8 @@ int main(void) {
 	}
 	// Inicializa o add-on para utilização de imagens
 	al_init_image_addon();
-
+	// Inicializa o add-on para utilização de linhas
+	al_init_primitives_addon();
 	// Criamos a nossa janela - dimensões de 640x480 px
 	janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
 	if (!janela) {
@@ -71,7 +100,10 @@ int main(void) {
 	imagem.bitmap = al_load_bitmap("Imgs/photo.bmp");
 	retangulo.bitmap = al_load_bitmap("Imgs/ret.bmp");
 	certo.bitmap = al_load_bitmap("Imgs/ok.bmp");
-	if (!imagem.bitmap || !retangulo.bitmap || !certo.bitmap) {
+	lobinho.bitmap = al_load_bitmap("Imgs/lobinho.png");
+	canudo.bitmap = al_load_bitmap("Imgs/canudo.jpg");
+	aquarela.bitmap = al_load_bitmap("Imgs/aquarela.png");
+	if (!imagem.bitmap || !retangulo.bitmap || !certo.bitmap || !lobinho.bitmap || !canudo.bitmap || !aquarela.bitmap) {
 		fprintf(stderr, "Falha ao iniciar imagem\n");
 		al_destroy_display(janela);
 		return -1;
@@ -84,6 +116,9 @@ int main(void) {
 		al_destroy_bitmap(imagem.bitmap);
 		al_destroy_bitmap(retangulo.bitmap);
 		al_destroy_bitmap(certo.bitmap);
+		al_destroy_bitmap(lobinho.bitmap);
+		al_destroy_bitmap(canudo.bitmap);
+		al_destroy_bitmap(aquarela.bitmap);
 		return -1;
 	}
 
@@ -94,6 +129,9 @@ int main(void) {
 		al_destroy_bitmap(imagem.bitmap);
 		al_destroy_bitmap(retangulo.bitmap);
 		al_destroy_bitmap(certo.bitmap);
+		al_destroy_bitmap(lobinho.bitmap);
+		al_destroy_bitmap(canudo.bitmap);
+		al_destroy_bitmap(aquarela.bitmap);
 		return -1;
 	}
 	// Atribui o cursor padrão do sistema para ser usado
@@ -103,6 +141,9 @@ int main(void) {
 		al_destroy_bitmap(imagem.bitmap);
 		al_destroy_bitmap(retangulo.bitmap);
 		al_destroy_bitmap(certo.bitmap);
+		al_destroy_bitmap(lobinho.bitmap);
+		al_destroy_bitmap(canudo.bitmap);
+		al_destroy_bitmap(aquarela.bitmap);
 		return -1;
 	}
 
@@ -115,12 +156,7 @@ int main(void) {
 
 	// Preenchemos a janela de branco
 	al_clear_to_color(al_map_rgb(255, 255, 255));
-
-	//desenha a imagem na tela
-	al_draw_bitmap(retangulo.bitmap, retangulo.x, retangulo.y, 0);
-	al_draw_bitmap(imagem.bitmap, imagem.x, imagem.y, 0);
-
-	// Atualiza a tela
+	
 	al_flip_display();
 
 	int gameOver = 0;
@@ -143,6 +179,45 @@ int main(void) {
 					Arrastando = 1;
 					imagem.cliqueX = MapearDistancia(evento.mouse.x, imagem.x);
 					imagem.cliqueY = MapearDistancia(evento.mouse.y, imagem.y);
+				}
+				else if (IsInside(evento.mouse.x, evento.mouse.y, lobinho) && !Arrastando) {
+					if (checaInv(invCount))
+					{
+						inventario[invCount++] = lobinho;
+						lobinho.bitmap = NULL;
+						lobinho.altura = 0;
+						lobinho.largura = 0;
+						lobinho.x = -1;
+						lobinho.y = -1;
+					}
+					else
+						printf("Inventario cheio");
+				}
+				else if (IsInside(evento.mouse.x, evento.mouse.y, canudo) && !Arrastando) {
+					if (checaInv(invCount))
+					{
+						inventario[invCount++] = canudo;
+						canudo.bitmap = NULL;
+						canudo.altura = 0;
+						canudo.largura = 0;
+						canudo.x = -1;
+						canudo.y = -1;
+					}
+					else
+						printf("Inventario cheio");
+				}
+				else if (IsInside(evento.mouse.x, evento.mouse.y, aquarela) && !Arrastando) {
+					if (checaInv(invCount))
+					{
+						inventario[invCount++] = aquarela;
+						aquarela.bitmap = NULL;
+						aquarela.altura = 0;
+						aquarela.largura = 0;
+						aquarela.x = -1;
+						aquarela.y = -1;
+					}
+					else
+						printf("Inventario cheio");
 				}
 				else
 				{
@@ -174,11 +249,26 @@ int main(void) {
 
 		al_clear_to_color(al_map_rgb(255, 255, 255));
 
+
 		al_draw_bitmap(retangulo.bitmap, retangulo.x, retangulo.y, 0);
 		al_draw_bitmap(imagem.bitmap, imagem.x, imagem.y, 0);
+		if(lobinho.bitmap)
+			al_draw_bitmap(lobinho.bitmap, lobinho.x, lobinho.y, 0);
+		if(canudo.bitmap)
+			al_draw_bitmap(canudo.bitmap, canudo.x, canudo.y, 0);
+		if(aquarela.bitmap)
+			al_draw_bitmap(aquarela.bitmap, aquarela.x, aquarela.y, 0);
 		if (correto) {
 			al_draw_bitmap(certo.bitmap, certo.x, certo.y, 0);
 		}
+
+		desenhaLinhaInventario();
+		for (c = 0; c < invCount; c++)
+		{
+			al_draw_scaled_bitmap(inventario[c].bitmap, 0, 0, inventario[c].largura, inventario[c].altura, c * LARGURA_TELA / 10, ALTURA_TELA - (inventario[c].altura * 0.4), inventario[c].largura * 0.4, inventario[c].altura * 0.4, 0);
+		}
+
+
 
 		al_flip_display();
 	}
@@ -189,9 +279,25 @@ int main(void) {
 	al_destroy_bitmap(imagem.bitmap);
 	al_destroy_bitmap(retangulo.bitmap);
 	al_destroy_bitmap(certo.bitmap);
+	al_destroy_bitmap(lobinho.bitmap);
+	al_destroy_bitmap(canudo.bitmap);
+	al_destroy_bitmap(aquarela.bitmap);
 
 	// Finaliza fila de eventos
 	al_destroy_event_queue(fila_eventos);
+
+	return 0;
+}
+
+int desenhaLinhaInventario()
+{
+	al_draw_line(0, ALTURA_TELA - 100, LARGURA_TELA, ALTURA_TELA - 100, al_map_rgb(255,0,0), 1);
+}
+
+int checaInv(int i)
+{
+	if (i < TAMANHO_INVENTARIO)
+		return 1;
 
 	return 0;
 }
